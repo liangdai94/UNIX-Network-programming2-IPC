ab99a6e9fe16d1c5238710918a912e6a1b29465f
liangdai94 <1029721846@qq.com>
2020-11-26 22:30:15 +0800

    Update mesg.h

--- SystemV/ch6_8_2/include/mesg.h
+++ SystemV/ch6_8_2/include/mesg.h
@@ -3,9 +3,13 @@
 
 #include"myhead.h"
 
-#define MAXMESGDATA (MAXLINE-2*sizeof(long))
 #define MQ_KEY1 1234L
+#define MQ_KEY2 2345L
+#define MSG_R 0400
+#define MSG_W 0200
+#define SVMSG (MSG_R | MSG_W | MSG_R >> 3 | MSG_W >> 3)
 
+#define MAXMESGDATA (MAXLINE-2*sizeof(long))
 #define MESHDRSIZE (sizeof(struct mymsg) - MAXMESGDATA)
 
 struct mymsg
